bfe065b3fc5a3619108cff56c627e8fb8d0ad008
Scott Hoisington <hoyzington@gmail.com>
2020-05-13 00:01:56 -0400

    Add articles index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources 'articles', only: [:show]
+  resources :articles, only: [:show, :index]
 
   get 'about', to: 'pages#about'
   root 'pages#home'
