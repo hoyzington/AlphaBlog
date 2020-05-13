31c5d49625729f4ebbca15a1047f31c643988fda
Scott Hoisington <hoyzington@gmail.com>
2020-05-12 23:43:19 -0400

    Add articles show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'pages#home'
+  resources 'articles', only: [:show]
 
   get 'about', to: 'pages#about'
+  root 'pages#home'
 
 end
