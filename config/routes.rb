681062b26e561edec70e87ef56d93221e096143b
Scott Hoisington <hoyzington@gmail.com>
2020-05-13 20:35:14 -0400

    Add CRU to articles model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 
   get 'about', to: 'pages#about'
   root 'pages#home'
