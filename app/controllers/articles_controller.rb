d2235ff4e10ca4e406848cefda4ed38bed9aedda
Scott Hoisington <hoyzington@gmail.com>
2020-05-15 19:57:33 -0400

    Add styling to nav & home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,11 @@ class ArticlesController < ApplicationController
 
   before_action :set_article, only:[:show, :edit, :update, :destroy]
 
-  def show
-  end
-
   def index
     @articles = Article.all
+  end
+
+  def show
   end
 
   def new
