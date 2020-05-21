8f92ebc2a6685926620999e97c2b9e6fd62d5223
Scott Hoisington <hoyzington@gmail.com>
2020-05-21 01:55:58 -0400

    Add user-article association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   
+  has_many :articles
+
   validates :username, presence: true,
                     uniqueness: {case_sensitive: false},
                     length: {minimum: 3, maximum: 25}
