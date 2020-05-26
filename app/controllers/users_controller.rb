41d3a484077aedddd0446fb497b198021110cf86
Scott Hoisington <hoyzington@gmail.com>
2020-05-26 01:01:38 -0400

    Add login/logout, user restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
@@ -16,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Thank you, #{@user.username}, for signing up!"
       redirect_to @user
     else
@@ -24,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account has been updated"
       redirect_to @user
@@ -41,6 +41,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user). permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   
   get 'about', to: 'pages#about'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 end
