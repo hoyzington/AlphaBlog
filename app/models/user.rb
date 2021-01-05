d4582c5a15a5de45ed3210fbfa3d014bf2b85335
Scott Hoisington <hoyzington@gmail.com>
2021-01-05 16:54:56 -0500

    add regexp for email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   validates :username, presence: true,
                     uniqueness: {case_sensitive: false},
                     length: {minimum: 3, maximum: 25}
-  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                  
+
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :email, presence: true,
                     uniqueness: {case_sensitive: false},
                     length: {maximum: 105},
